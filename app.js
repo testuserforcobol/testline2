e0018dbdfb6fe72e08ffabce487642e811beea1d
TEST USER FOR COBOL PROGRAM <33194248+testuserforcobol@users.noreply.github.com>
2017-11-05 21:15:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const line = require('@line/bot-sdk');
 const express = require('express');
 const cobolscript = require('cobolscript');
 
-
 const hookConsoleLog = (logs) => {
     console.log = (d) => logs.push(d);
 };
@@ -39,16 +38,27 @@ function handleEvent(event) {
     return Promise.resolve(null);
   }
 
-  var logs = [];
-  var program = cobolscript.compileProgramFile('./hello.cob');
+  var webclient = require('request');
+  webclient.post({
+    url: 'https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk',
+    form: { apikey: process.env.a3rt_talk_apikey, query: event.message.text },
+    json: true
+  }, function (err, response, body) {
+    var logs = [];
+    var program = cobolscript.compileProgramFile('./hello.cob');
+    hookConsoleLog(logs);
+    program.run(cobolscript.getRuntime());
 
-  hookConsoleLog(logs);
-  program.run(cobolscript.getRuntime());
-
-  var msgtext = event.message.text + '\n' + logs.join('\n'); 
-  const echo = { type: 'text', text: msgtext };
-
-  return client.replyMessage(event.replyToken, echo);
+    var a3rtMsg;
+    if (body.status == 0) {
+        a3rtMsg = body.results[0].reply;
+    } else {
+        a3rtMsg = event.message.text;
+    }
+    var msgtext = a3rtMsg + '\n' + logs.join('\n');
+    const echo = { type: 'text', text: msgtext };
+    return client.replyMessage(event.replyToken, echo);
+  });
 }
 
 const port = process.env.PORT || 3000;
